162adfc55edc2c4816c0982e14f8f7598e47463c
Thomas Sailer <sailer@tsailer.ch>
2021-06-21 23:11:17 +0200

    correct issue rate

--- gcc/config/riscv/riscv.c
+++ gcc/config/riscv/riscv.c
@@ -307,7 +307,7 @@ static const struct riscv_tune_param amethyst_tune_info = {
   {COSTS_N_INSNS (20), COSTS_N_INSNS (20)},	/* fp_div */
   {COSTS_N_INSNS (2), COSTS_N_INSNS (2)},	/* int_mul */
   {COSTS_N_INSNS (6), COSTS_N_INSNS (6)},	/* int_div */
-  1,						/* issue_rate */
+  2,						/* issue_rate */
   3,						/* branch_cost */
   2,						/* memory_cost */
   false,					/* slow_unaligned_access */
